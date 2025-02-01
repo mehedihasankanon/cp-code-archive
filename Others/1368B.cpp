4a6eca6297ad0bf9e5d06ff8f59544c4a8b0b6d9
Mehedi Hasan Kanon <166309074+mehedihasankanon@users.noreply.github.com>
2025-02-01 14:01:05 +0600

    Add files via upload

--- Others/1368B.cpp
+++ Others/1368B.cpp
@@ -1,7 +1,7 @@
 // Bismillahir Rahmanir Rahim
      
 // link    : https://codeforces.com/contest/2049/problem/C
-// status  : 
+// status  : **editorial**
      
 #pragma GCC optimize("O1,O2,O3")
 #pragma GCC optimize("Ofast,unroll-loops")
@@ -27,11 +27,27 @@ ll INF = 0x7fffffffffffffffLL;
     
 ll testcase = 1;
 
+
+// vector<ll> pwrs;
 void Pre(void)
 {
     
-    
-    
+    // cerr << powl(40.0,10.0);
+    // 40 it is!
+
+    // pwrs.push_back(0);
+    // forn(i,1,41) 
+    // {
+    //     ll p = 9, b = i;
+    //     while(p--) b *= i;
+    //     // dbg(b);
+    //     // cerr << NL;
+    //     pwrs.push_back(b);
+    //     // if(b >= 0) pwrs.push_back(b);
+    // }
+
+    // for(auto x : pwrs) dbg(x);
+
     return;
 }   
     
@@ -39,16 +55,79 @@ void Pre(void)
 void Solve(ll test)
 {
     ll n; cin >> n;
-    n--;
-    cout << "codeforces";
-    forn(i,0,n) cout << "s";
+
+    string s = "codeforces";
+    if(n == 1)
+    {
+        cout << "codeforces" << NL;
+        return;
+    }
+
+    vector<ll> v(10,1);
+    ll p = 1;
+
+    while(p < n)
+    {
+        forn(i,0,10)
+        {
+            if(p >= n) break;
+            p /= v[i];
+            v[i]++;
+            p *= v[i];
+        }
+    }
+
+    forn(i,0,10)
+    {
+        forn(j,0,v[i]) cout << s[i];
+    }
+
+    // ll t = 0;
+    // while(t < 40 && n >= pwrs[t]) t++;
+    // t++;
+    // if(pwrs[t - 1] > n) t--;
+    // // dbg(pwrs[t]);
+    // // dbg(pwrs[t - 1]);
+    // // dbg(pwrs[t - 2]);
+
+    // // dbg(t);
+    // ll mx = 1, c = t;
+    // while(c < n) 
+    // {
+    //     mx++; c *= t;
+    // }
+
+
+
+    // if(mx>0) forn(i,0,t)cout << "c"; else cout << "c";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "o"; else cout << "o";    
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "d"; else cout << "d";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "e"; else cout << "e";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "f"; else cout << "f";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "o"; else cout << "o";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "r"; else cout << "r";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "c"; else cout << "c";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "e"; else cout << "e";
+    // mx--;
+    // if(mx>0) forn(i,0,t)cout << "s"; else cout << "s";
+    // mx--;
+    
+
     return;
 }   
     
 int main()
 {   
-    ios_base::sync_with_stdio(false); 
-    cin.tie(nullptr); cout.tie(nullptr);
+    // ios_base::sync_with_stdio(false); 
+    // cin.tie(nullptr); cout.tie(nullptr);
     Pre();
     
     // cin >> testcase;
@@ -56,6 +135,7 @@ int main()
     ll test;
     for(test = 1; test <= testcase; test++) {
         Solve(test);
+        // ;
     }
     return 0;
 }   
