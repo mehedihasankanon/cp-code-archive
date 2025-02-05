8a3997a9780369454de202d44444b6ef32c02940
Mehedi Hasan Kanon <mhkanon.mzs1853@gmail.com>
2025-02-05 09:10:15 -0500

    Added files via upload

--- Others/tempCodeRunnerFile.cpp
+++ Others/tempCodeRunnerFile.cpp
@@ -1,2 +1,4 @@
+---------" << NL;
+        // cerr << max(mid * b1 - nb,0LL) * pb + max(mid * s1 - ns,0LL) * ps + max(mid * c1 - nc,0LL) * pc << NL;
 
-    freopen("output.out", "w", stdout);
+        // dbg(lo); dbg(hi);
