d93b4c8bf8b8540b58e796975ea6fff4d6659597
mhkanon <mehedi.hkanon@gmail.com>
2025-06-24 16:10:22 +0600

    Last one i promise :)

---
+++ All_codes/C_Equilateral_Triangle.cpp
@@ -0,0 +1,101 @@
+// Bismillahir rahmanir rahim
+
+// https://atcoder.jp/contests/abc409/tasks/abc409_c
+// AC
+
+#pragma GCC optimize("O3,unroll-loops")
+
+#include <bits/stdc++.h>
+
+// #pragma GCC target("avx2")
+
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+// template<typename T>
+// using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// Extra functionality :
+// os.find_by_order(index) => iterator to the element at that index (0-based)
+// os.order_of_key(value) => number of elements strictly less than value
+
+using ll = long long;
+// using ll = int;
+using ld = long double;
+
+ll mod = 1000000007;
+// ll mod = 998244353;
+
+#define nl "\n"
+#define forn(i,s,e) for(ll i = s; i < e; i++)
+#define forr(i,s,e) for(ll i = s; i >= e; i--)
+
+
+// #define DEBUG
+
+#ifdef DEBUG
+#define dbg(n) cerr<<__LINE__<<" "<<#n<<" "<<n<<endl;
+#define dbgc(a) cerr<<__LINE__<<" "<<#a<<" "<<'['<<" ";for(auto el:a){cerr<<el<<" ";}cerr<<']'<<endl;
+#define dbgcc(a) cerr<<__LINE__<<" "<<#a<<" "<<'['<<" ";for(auto el:a){cerr<<'{'<<" "<<el.first<<','<<el.second<<" "<<'}'<<" ";}cerr<<']'<<endl;
+#else
+#define dbg(n) // single variable, string
+#define dbgc(a) // vector, deque, array
+#define dbgcc(a) // map, vector<pll>
+#endif
+
+ll testcase = 1;
+
+void Pre(void)
+{
+
+    return;
+}   
+
+
+void Solve(ll test)
+{
+    ll n, l; cin >> n >> l;
+    if(l % 3)
+    {
+        cout << 0 << nl;
+        return;
+    }
+
+    vector<ll> v(l + 2, 0);
+    v[0]++;
+    ll t = 0;
+    forn(i,0,n-1)
+    {
+        ll x; cin >> x;
+        t += x;
+        t %= l;
+        v[t]++;
+    }
+
+    l /= 3;
+    ll ans = 0;
+    forn(i,0,l)
+    {
+        ans += (v[i] * v[i+l] * v[i + 2 * l]);
+    }
+    cout << ans << nl;
+    return;
+}
+
+int main()
+{   
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr); cout.tie(nullptr);
+    Pre();
+    
+    // cin >> testcase;
+    
+    ll test;
+    for(test = 1; test <= testcase; test++) {
+        Solve(test);
+    }
+    return 0;
+}
