41c9979cb0632d450f4ae0b2d14a9d47a2acce53
Mehedi Hasan Kanon <mehedi.hkanon@gmail.com>
2025-07-14 21:05:40 +0600

    14 July 2025

---
+++ All_codes/B_Geometric_Sequence.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/abc390/tasks/abc390_b
+
+def main():
+    n = int(input())
+    
+    a = list(map(int, input().split()))
+
+    for i in range(1, n-1):
+        if a[i-1] * a[i+1] != a[i] ** 2:
+            print("No")
+            return
+    
+    print("Yes")
+
+main()
